e79804c09b474a493da2ae10672ecf0d4dcab7e2
unknown <revanthsomu9@gmail.com>
2023-02-26 11:10:41 +0530

    setup gh-pages

---
+++ apple/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ironman from './ironman.png'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={ironman}></img>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
